13bedc8ef2c1c93af33e44982876850b18f04f70
Vincent Labatut <vince.labatut@gmail.com>
2013-06-21 16:27:57 +0300

    connectivity disabled

--- global-properties-connectivity.R
+++ global-properties-connectivity.R
@@ -1,24 +1,25 @@
 #################################
 # connectivity-related
 #################################
-properties[["link connectivity average"]] <- list(
-	type=numeric(),
-	bounds=c(0,NA),
-	foo=function(graph) 
-	{	if(length(cache$linkconnectivity)==0)
-			cache$linkconnectivity <<- edge.connectivity(graph=graph, source=V(g), target=V(g), checks=TRUE)
-		mean(cache$linkconnectivity,na.rm=TRUE)
-	}
-)
-properties[["link connectivity stdev"]] <- list(
-	type=numeric(),
-	bounds=c(0,NA),
-	foo=function(graph) 
-	{	if(length(cache$linkconnectivity)==0)
-			cache$linkconnectivity <<- edge.connectivity(graph=graph, source=V(g), target=V(g), checks=TRUE)
-		sd(cache$linkconnectivity,na.rm=TRUE)
-	}
-)
+#TODO:   At foreign-graphml.c:373 :Could not add vertex ids, there is already an 'id' vertex attribute
+#properties[["link connectivity average"]] <- list(
+#	type=numeric(),
+#	bounds=c(0,NA),
+#	foo=function(graph) 
+#	{	if(length(cache$linkconnectivity)==0)
+#			cache$linkconnectivity <<- edge.connectivity(graph=graph, source=V(g), target=V(g), checks=TRUE)
+#		mean(cache$linkconnectivity,na.rm=TRUE)
+#	}
+#)
+#properties[["link connectivity stdev"]] <- list(
+#	type=numeric(),
+#	bounds=c(0,NA),
+#	foo=function(graph) 
+#	{	if(length(cache$linkconnectivity)==0)
+#			cache$linkconnectivity <<- edge.connectivity(graph=graph, source=V(g), target=V(g), checks=TRUE)
+#		sd(cache$linkconnectivity,na.rm=TRUE)
+#	}
+#)
 properties[["adhesion"]] <- list(		# aka minimal link connectivity
 	type=integer(),
 	bounds=c(0,NA),
@@ -26,33 +27,33 @@ properties[["adhesion"]] <- list(		# aka minimal link connectivity
 	{	graph.adhesion(graph=graph, checks=TRUE)
 	}
 )
-properties[["link connectivity max"]] <- list(
-	type=integer(),
-	bounds=c(0,NA),
-	foo=function(graph) 
-	{	if(length(cache$linkconnectivity)==0)
-			cache$linkconnectivity <<- edge.connectivity(graph=graph, source=V(g), target=V(g), checks=TRUE)
-		max(cache$linkconnectivity,na.rm=TRUE)
-	}
-)
-properties[["node connectivity average"]] <- list(
-	type=numeric(),
-	bounds=c(0,NA),
-	foo=function(graph) 
-	{	if(length(cache$nodeconnectivity)==0)
-			cache$nodeconnectivity <<- vertex.connectivity(graph=graph, source=V(g), target=V(g), checks=TRUE)
-		mean(cache$nodeconnectivity,na.rm=TRUE)
-	}
-)
-properties[["node connectivity stdev"]] <- list(
-	type=numeric(),
-	bounds=c(0,NA),
-	foo=function(graph) 
-	{	if(length(cache$nodeconnectivity)==0)
-			cache$nodeconnectivity <<- vertex.connectivity(graph=graph, source=V(g), target=V(g), checks=TRUE)
-		sd(cache$nodeconnectivity,na.rm=TRUE)
-	}
-)
+#properties[["link connectivity max"]] <- list(
+#	type=integer(),
+#	bounds=c(0,NA),
+#	foo=function(graph) 
+#	{	if(length(cache$linkconnectivity)==0)
+#			cache$linkconnectivity <<- edge.connectivity(graph=graph, source=V(g), target=V(g), checks=TRUE)
+#		max(cache$linkconnectivity,na.rm=TRUE)
+#	}
+#)
+#properties[["node connectivity average"]] <- list(
+#	type=numeric(),
+#	bounds=c(0,NA),
+#	foo=function(graph) 
+#	{	if(length(cache$nodeconnectivity)==0)
+#			cache$nodeconnectivity <<- vertex.connectivity(graph=graph, source=V(g), target=V(g), checks=TRUE)
+#		mean(cache$nodeconnectivity,na.rm=TRUE)
+#	}
+#)
+#properties[["node connectivity stdev"]] <- list(
+#	type=numeric(),
+#	bounds=c(0,NA),
+#	foo=function(graph) 
+#	{	if(length(cache$nodeconnectivity)==0)
+#			cache$nodeconnectivity <<- vertex.connectivity(graph=graph, source=V(g), target=V(g), checks=TRUE)
+#		sd(cache$nodeconnectivity,na.rm=TRUE)
+#	}
+#)
 properties[["cohesion"]] <- list(		# aka minimal node connectivity
 	type=integer(),
 	bounds=c(0,NA),
@@ -60,12 +61,12 @@ properties[["cohesion"]] <- list(		# aka minimal node connectivity
 	{	graph.cohesion(graph=graph, checks=TRUE)
 	}
 )
-properties[["node connectivity max"]] <- list(
-	type=integer(),
-	bounds=c(0,NA),
-	foo=function(graph) 
-	{	if(length(cache$nodeconnectivity)==0)
-			cache$nodeconnectivity <<- vertex.connectivity(graph=graph, source=V(g), target=V(g), checks=TRUE)
-		max(cache$nodeconnectivity,na.rm=TRUE)
-	}
-)
+#properties[["node connectivity max"]] <- list(
+#	type=integer(),
+#	bounds=c(0,NA),
+#	foo=function(graph) 
+#	{	if(length(cache$nodeconnectivity)==0)
+#			cache$nodeconnectivity <<- vertex.connectivity(graph=graph, source=V(g), target=V(g), checks=TRUE)
+#		max(cache$nodeconnectivity,na.rm=TRUE)
+#	}
+#)
