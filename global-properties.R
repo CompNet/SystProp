ddab6a860a92b53d0b09a11b1eb0e44222ffc6fb
Vincent Labatut <vince.labatut@gmail.com>
2013-07-02 14:15:23 +0300

    additional comments

--- global-properties.R
+++ global-properties.R
@@ -19,7 +19,9 @@ if(os=="windows")
 }else
 {	data.folder <- "/var/data/networks/"
 #	folders <- c(1,10,100,101,106,107,108,109,110,112,113,114,115,119,123,124,125,126,127,128,129,13,130,131,132,133,134,135,136,137,138,139,14,140,146,147,148,15,152,153,154,155,159,166,167,168,17,171,172,173,174,175,178,179,180,181,182,2,20,201,202,203,204,205,206,207,208,209,21,210,211,212,213,214,215,216,227,228,229,23,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,246,247,248,249,250,26,29,3,30,31,32,33,34,35,36,38,41,43,44,45,46,47,48,49,5,50,51,52,53,54,55,6,7,73,75,76,77,78,79,8,80,81,82,83,84,9,90,91,92,93,94,95,96,97,98,99)
-	folders <- 1:15	#1:297
+#	folders <- 100:297	#1:297
+	#TODO pb: 22, 27, 28, 37, 38, 39, 40, 42, 56, 57, 58, 59, 60, 62
+	# 63, 64, 65, 66, 67, 68
 }
 plot.folder <- paste(data.folder,"plots/",sep="")
 
@@ -32,20 +34,22 @@ plot.folder <- paste(data.folder,"plots/",sep="")
 # TODO hop plots
 # TODO p value of the power law distribution test
 # TODO stats on embeddedness
+# TODO whether the graph is bipartite or not
+# TODO multiplex, attributed, 
 properties <- list()
 source("SystProp/global-properties-general.R")
 source("SystProp/global-properties-element.R")
 source("SystProp/global-properties-component.R")
 source("SystProp/global-properties-degree.R")
-source("SystProp/global-properties-distance.R")
+#source("SystProp/global-properties-distance.R")
 source("SystProp/global-properties-transitivity.R")
-source("SystProp/global-properties-betweenness.R")
-source("SystProp/global-properties-closeness.R")
-source("SystProp/global-properties-edgebetweenness.R")
+#source("SystProp/global-properties-betweenness.R")
+#source("SystProp/global-properties-closeness.R")
+#source("SystProp/global-properties-edgebetweenness.R")
 source("SystProp/global-properties-spectral.R")
-source("SystProp/global-properties-connectivity.R")
-source("SystProp/global-properties-eccentricity.R")
-source("SystProp/global-properties-community.R")
+#source("SystProp/global-properties-connectivity.R")
+#source("SystProp/global-properties-eccentricity.R")
+#source("SystProp/global-properties-community.R")
 
 #################################
 # init/load data frame
