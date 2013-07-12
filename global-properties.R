5955f928e4de4836226794ecd3a226225b4552d3
Vincent Labatut <vince.labatut@gmail.com>
2013-07-12 13:11:43 +0300

    recording after each measure

--- global-properties.R
+++ global-properties.R
@@ -20,7 +20,7 @@ if(os=="windows")
 }else
 {	data.folder <- "/var/data/networks/"
 #	folders <- c(1,10,100,101,106,107,108,109,110,112,113,114,115,119,123,124,125,126,127,128,129,13,130,131,132,133,134,135,136,137,138,139,14,140,146,147,148,15,152,153,154,155,159,166,167,168,17,171,172,173,174,175,178,179,180,181,182,2,20,201,202,203,204,205,206,207,208,209,21,210,211,212,213,214,215,216,227,228,229,23,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,246,247,248,249,250,26,29,3,30,31,32,33,34,35,36,38,41,43,44,45,46,47,48,49,5,50,51,52,53,54,55,6,7,73,75,76,77,78,79,8,80,81,82,83,84,9,90,91,92,93,94,95,96,97,98,99)
-	folders <- c(1:17, 19:57, 59:73, 75:98)	#1:297
+	folders <- c(1:17, 19:57, 59:71, 73, 75:98)	#1:297
 }
 plot.folder <- paste(data.folder,"plots/",sep="")
 
@@ -136,16 +136,16 @@ for(f in folders)
 						data[as.character(f),prop.names[p]] <- Inf
 				end.time <- Sys.time();
 				total.time <- end.time - start.time;
-				cat("[",format(end.time,"%a %d %b %Y %X"),"] .. Processing completed in ",total.time,": ",data[as.character(f),prop.names[p]],"\n",sep="")
+				cat("[",format(end.time,"%a %d %b %Y %X"),"] ..Processing completed in ",total.time,": ",data[as.character(f),prop.names[p]],"\n",sep="")
+				
+				# write resulting table
+				cat("[",format(Sys.time(),"%a %d %b %Y %X"),"] ..Update property files\n",sep="")
+				write.table(x=data, file=table.file)
 			}
 		}
 		end.time0 <- Sys.time();
 		total.time0 <- end.time0 - start.time0;
 		cat("[",format(end.time0,"%a %d %b %Y %X"),"] Processing completed in ",total.time0,"\n",sep="")
-		
-		# write resulting table
-		cat("[",format(Sys.time(),"%a %d %b %Y %X"),"] Update property files\n",sep="")
-		write.table(x=data, file=table.file, )
 	}
 }
 
